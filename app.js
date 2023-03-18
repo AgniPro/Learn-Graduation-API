d9226d0b25bfb9a7ba501bf9d44a75c49dd5ff83
AgniPro <84276343+AgniPro@users.noreply.github.com>
2023-03-18 19:29:01 +0530

    2nd upload

--- app.js
+++ app.js
@@ -3,112 +3,78 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const homeStartingContent = "Application are under progress so please visit to learngraduation.blogspot.com or click on below link"
-const contactContent = "Contact us at agnipro(at)gmail(dot)com"
-const aboutContent = "this is blog post templet designed by agnipro and the creator is abhidhek mehta"
-
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
-mongoose.connect("mongodb+srv://agnipro:<agnipro%20>@agnipro.absogmm.mongodb.net/?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-});
+mongoose.connect("mongodb+srv://agnipro:agnipro%40@agnipro.absogmm.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true});
 
 const postSchema = {
-    _id:String,
-    title: String,
-    content: String
-}
+  title: String,
+  content: String
+};
+
 const Post = mongoose.model("Post", postSchema);
 
+app.get("/", function(req, res){
 
-app.get("/", function (req, res) {
-
-    Post.find({}, function (err, posts) {
-        res.render("home", {
-            startingContent: homeStartingContent,
-            posts: posts,
-        });
-    });
-
-});
-app.get("/contact", function (req, res) {
-    res.render("contact", {
-        contactpg: contactContent
-    });
-
-});
-app.get("/about", function (req, res) {
-    res.render("about", {
-        aboutpg: aboutContent
-    });
-
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
+  });
 });
 
-app.get("/sign", function (req, res) {
-    res.render("sign");
+app.get("/compose", function(req, res){
+  res.render("compose");
 });
 
-app.post("/sign", function (req, res) {
-    const sId = "abhishek";
-    const sPs = "1234"
-    if (req.body.userId === sId && req.body.password === sPs) {
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
 
-        app.get("/compose", function (req, res) {
-            res.render("compose");
-        });
 
-        res.redirect("/compose");
-
-        app.post("/compose", function(req, res){
-            const post = new Post({
-               _id: req.body.purl,
-              title: req.body.postTitle,
-              content: req.body.postBody
-            });
-          
-          
-            post.save(function(err){
-              if (!err){
-                  res.redirect("/");
-              }
-            });
-        });
-    } else {
-        res.redirect("/sign");
-
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
     }
-
+  });
 });
-
 
 app.get("/posts/:postId", function(req, res){
 
-    const requestedPostId = req.params.postId;
+const requestedPostId = req.params.postId;
 
-      Post.findOne({_id: requestedPostId}, function(err, post){
-        res.render("post", {
-            _id: post.purl,
-          title: post.title,
-          content: post.content,
-          
-        });
-      });
-    
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
 });
 
 
-app.listen(3000, function () {
-    console.log("server has started");
-
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
