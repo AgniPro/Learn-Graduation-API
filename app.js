9f7187e3cc22c6408c972323c5c2cf2ee096cc73
AgniPro <84276343+AgniPro@users.noreply.github.com>
2023-03-22 18:09:16 +0530

    ver 1.2
    adding google authenticator

--- app.js
+++ app.js
@@ -1,80 +1,238 @@
 //jshint esversion:6
 
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const sesssion = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
+const _ = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Application are under progress so please visit to learngraduation.blogspot.com or click on below link"
+const contactContent = "Contact us at agnipro(at)gmail(dot)com"
+const aboutContent = "this is blog post templet designed by agnipro and the creator is abhidhek mehta"
+
 
 const app = express();
-
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://agnipro:agnipro%40@agnipro.absogmm.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true});
+app.use(sesssion({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+mongoose.connect("mongodb+srv://agnipro:agnipro%40@agnipro.absogmm.mongodb.net/learngraduation", {
+    useNewUrlParser: true
+});
 
 const postSchema = {
-  title: String,
-  content: String
-};
-
+    _id: String,
+    title: String,
+    content: String
+}
 const Post = mongoose.model("Post", postSchema);
 
-app.get("/", function(req, res){
 
-  Post.find({}, function(err, posts){
-    res.render("home", {
-      startingContent: homeStartingContent,
-      posts: posts
-      });
-  });
-});
+app.get("/", function (req, res) {
 
-app.get("/compose", function(req, res){
-  res.render("compose");
-});
-
-app.post("/compose", function(req, res){
-  const post = new Post({
-    title: req.body.postTitle,
-    content: req.body.postBody
-  });
-
-
-  post.save(function(err){
-    if (!err){
-        res.redirect("/");
-    }
-  });
-});
-
-app.get("/posts/:postId", function(req, res){
-
-const requestedPostId = req.params.postId;
-
-  Post.findOne({_id: requestedPostId}, function(err, post){
-    res.render("post", {
-      title: post.title,
-      content: post.content
+    Post.find({}, function (err, posts) {
+        res.render("home", {
+            startingContent: homeStartingContent,
+            posts: posts,
+        });
     });
-  });
+
+});
+app.get("/contact", function (req, res) {
+    res.render("contact", {
+        contactpg: contactContent
+    });
+
+});
+app.get("/about", function (req, res) {
+    res.render("about", {
+        aboutpg: aboutContent
+    });
 
 });
 
-app.get("/about", function(req, res){
-  res.render("about", {aboutContent: aboutContent});
+app.get("/login", function (req, res) {
+    res.render("login");
 });
 
-app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+app.get("/register", function (req, res) {
+    res.render("register");
+
+});
+
+app.get("/", function (req, res) {
+    User.find({"/":{$ne:null}}, function(err, foundUser){
+     if(err){
+         console.log(err);
+     }else{
+         if (foundUser){
+             res.render("/", {userslogin: foundUser})
+         }
+     }
+    });
+ 
+ });
+ 
+ app.get("/compose", function (req, res) {
+    if (req.isAuthenticated()) {
+        res.render("compose");
+    } else {
+        res.redirect("/login");
+    }
+
+});
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    googleId: String,
+    secret: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: process.env.CALLBACK_URL,
+        passReqToCallback: true
+    },
+    function (request, accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({
+            googleId: profile.id
+        }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
+app.get("/auth/google",
+    passport.authenticate("google", {
+        scope: ["email", "profile"]
+    }));
+
+app.get("/auth/google/compose",
+    passport.authenticate("google", {
+        successRedirect: "/compose",
+        failureRedirect: "/login"
+    }));
+
+app.get("/logout", function (req, res) {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+
+});
+
+app.post("/register", function (req, res) {
+    User.register({
+        username: req.body.username
+    }, req.body.password, function (err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("/");
+            })
+        }
+    })
+
+});
+
+app.post("/login", function (req, res) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, function () {
+                res.redirect("compose");
+            });
+        }
+    });
+});
+
+app.post("/submit", function (req, res) {
+    const submittedSecret = req.body.secret;
+
+    //Once the user is authenticated and their session gets saved, their user details are saved to req.user.
+    // console.log(req.user.id);
+
+    User.findById(req.user.id, function (err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function () {
+                    res.redirect("/");
+                });
+            }
+        }
+    });
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.get("/posts/:postId", function (req, res) {
+
+    const requestedPostId = req.params.postId;
+
+    Post.findOne({
+        _id: requestedPostId
+    }, function (err, post) {
+        res.render("post", {
+            _id: post.purl,
+            title: post.title,
+            content: post.content,
+
+        });
+    });
+
+});
+
+
+app.listen(3000, function () {
+    console.log("server has started");
+
 });
