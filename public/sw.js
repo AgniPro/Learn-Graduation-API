1dede8bd6b9c2c2f2329b20ed62838456b721398
AgniPro <84276343+AgniPro@users.noreply.github.com>
2023-03-24 11:20:41 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,9 @@
-import Scripts("https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js");
-if(workbox){workbox.setConfig({debug:false});workbox.skipWaiting();workbox.clientsClaim();workbox.routing.registerRoute(new RegExp('.*'),workbox.strategies.networkFirst());}
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.3.1/workbox-sw.js");
+if (workbox) {
+    workbox.setConfig({
+        debug: false
+    });
+    workbox.skipWaiting();
+    workbox.clientsClaim();
+    workbox.routing.registerRoute(new RegExp('.*'), workbox.strategies.networkFirst());
+}
