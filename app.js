c0efdeb86a886ed0d3511416a9a3e213d566b924
AgniPro <84276343+AgniPro@users.noreply.github.com>
2023-04-21 20:12:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(passport.session());
 
 
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb+srv://agnipro:agnipro7257278@agnipro.absogmm.mongodb.net/learngraduation?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://agnipro:agnipro7257278@agnipro.absogmm.mongodb.net/learngraduation", {
     useNewUrlParser: true
 });
 
