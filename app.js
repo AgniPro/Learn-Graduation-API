62c4a409dc697dffed61571348d958e9698754f5
AgniPro <84276343+AgniPro@users.noreply.github.com>
2023-05-16 18:23:56 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -270,7 +270,7 @@ app.post("/update",function(req,res){
 
 app.post("/delete", function(req,res){
     if (req.isAuthenticated()){
-        const postid = req.body.del;
+        const postid = req.body.url;
         
         Post.findOneAndDelete({"url": postid}, (err, doc) => {
         if (err) {
