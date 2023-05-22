2c7a8494a0b1f578c7afe56ac18f2062afe194bd
AgniPro <84276343+AgniPro@users.noreply.github.com>
2023-05-22 15:33:02 +0530

    cookie to false

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
       maxAge: 60 * 60 * 1000,
-      secure: true // set to true if using HTTPS
+      secure: false // set to true if using HTTPS
     }
   }));
   
