3cbb5849d53ee4aa85bc687ffbc2adc8384ac308
AgniPro <84276343+AgniPro@users.noreply.github.com>
2023-07-02 16:49:23 +0530

    adding console.log and client url

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(sesssion({
     maxAge: 60 * 60 * 1000,
     sameSite: 'none',
     secure: true,
-    domain: '.learngraduation.web.app'
+    domain: 'https://learngraduation.web.app'
   }
 }));
 
