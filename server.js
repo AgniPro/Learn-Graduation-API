4d59e8d991727226ee2744e85160bb23a90a526f
AgniPro <84276343+AgniPro@users.noreply.github.com>
2023-07-02 17:06:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(sesssion({
     maxAge: 60 * 60 * 1000,
     sameSite: 'none',
     secure: true,
-    domain: 'https://learngraduation.web.app'
+    domain: '.learngraduation.web'
   }
 }));
 
