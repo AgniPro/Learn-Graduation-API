fc293a1601878a6a552aa90a2097e9d93fa7c2c3
AgniPro <84276343+AgniPro@users.noreply.github.com>
2023-07-03 17:32:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(session({
         maxAge: 60 * 60 * 1000,
         sameSite: 'none',
         secure: true,
-        domain:"learngraduation.onrender.com",
+        domain:'.onrender.com',
     } 
 }));
 
