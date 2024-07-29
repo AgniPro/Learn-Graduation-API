26c7b77549385f6ce6e7c38da6260375bc0aeff0
Abhishek Kumar Mehta <84276343+AgniPro@users.noreply.github.com>
2024-07-29 08:21:34 +0530

    updating google strategy

--- config/google-strategy.js
+++ config/google-strategy.js
@@ -7,7 +7,7 @@ import bcrypt from 'bcrypt'
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
 },
   async (accessToken, refreshToken, profile, done) => {
     try {
