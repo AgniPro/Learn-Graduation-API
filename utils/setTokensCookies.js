3aead00a9d420519852ba4014fd035de467a6a20
Abhishek Kumar Mehta <84276343+AgniPro@users.noreply.github.com>
2024-07-29 12:21:42 +0530

    Update setTokensCookies.js

--- utils/setTokensCookies.js
+++ utils/setTokensCookies.js
@@ -2,6 +2,7 @@ const setTokensCookies = (res, accessToken, refreshToken, newAccessTokenExp, new
   const accessTokenMaxAge = (newAccessTokenExp - Math.floor(Date.now() / 1000)) * 1000;
   const refreshTokenmaxAge = (newRefreshTokenExp - Math.floor(Date.now() / 1000)) * 1000;
   const isSecure = process.env.NODE_ENV === 'production' ? true : false;
+  const domain = process.env.FRONTEND_HOST;
   // Set Cookie for Access Token
   res.cookie('accessToken', accessToken, {
     httpOnly: true,
@@ -23,12 +24,14 @@ const setTokensCookies = (res, accessToken, refreshToken, newAccessTokenExp, new
     secure: isSecure, // Set to true if using HTTPS
     maxAge: refreshTokenmaxAge,
     sameSite: 'strict', 
+    domain: domain,
   });
   res.cookie('uid',uid, {
     httpOnly: false,
     secure: isSecure, // Set to true if using HTTPS
     maxAge: refreshTokenmaxAge,
     sameSite: 'strict',
+    domain: domain,
   });
 }
 
